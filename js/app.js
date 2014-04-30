fc6c33e4026d3ab8b44b17c94b6d8782f557e598
Jon Evans <jle@foraker.com>
2014-04-29 21:08:44 -0600

    Use ember data for data source

--- js/app.js
+++ js/app.js
@@ -2,42 +2,46 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
-App.PRODUCTS = [
-  {
-    title: 'Flint',
-    price: 99,
-    description: 'Flint is a thing',
-    isOnSale: true,
-    image: 'flint.png'
-  },
-  {
-    title: 'Kindling',
-    price: 249,
-    description: 'Kindling is a thing',
-    isOnSale: false,
-    image: 'kindling.png'
-  }
-]
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string')
+});
+
+/* Ember data:
+
+HTTP server with JSON (default)
+App.ApplicationAdapter = DS.RESTAdapter.extend();
+
+From memory:
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+*/
 
 App.Router.map(function() {
   this.route('about');
   this.route('about', { path: '/about_us' });
   this.resource('products', function() {
-    this.resource('product', { path: '/:title' })
+    this.resource('product', { path: '/:product_id' });
   });
 });
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
 
+/* Default Route:
 App.ProductRoute = Ember.Route.extend({
   model: function(params) {
-    return App.PRODUCTS.findBy('title', params.title);
+    return this.store.find('product', params.product_id);
   }
 })
+*/
 
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -55,3 +59,22 @@ App.AboutController = Ember.Controller.extend({
     return status;
   }.property()
 });
+
+App.Product.FIXTURES = [
+  {
+    id:          1,
+    title:       'Flint',
+    price:       99,
+    description: 'Flint is a thing',
+    isOnSale:    true,
+    image:       'flint.png'
+  },
+  {
+    id:          2,
+    title:       'Kindling',
+    price:       249,
+    description: 'Kindling is a thing',
+    isOnSale:    false,
+    image:       'kindling.png'
+  }
+]
